77d98866ddc8ccf13ba705df34395ba4448b092b
Chris Geoga <geoga@wisc.edu>
2026-01-30 14:57:29 -0600

    update simple 1D demo, but a few tests are failing.

--- scripts/simple_1d.jl
+++ scripts/simple_1d.jl
@@ -3,15 +3,12 @@ using SpectralKernels
 
 const ALPHA = inv(50.0)
 sdf(w)      = exp(-abs(w)*ALPHA)
-truth(r)    = 2*ALPHA/(ALPHA^2 + (2*pi*r)^2)
+cfg         = SpectralKernels.AdaptiveKernelConfig(sdf; tol=1e-12)
 
-integrand = SpectralKernels.Integrand(sdf)
-cfg       = SpectralKernels.AdaptiveKernelConfig(;tol=1e-12)
-
-xgrid       = collect(range(0.001, 1.1, length=1_000))
-true_values = truth.(xgrid)
-(integrals, errors) = kernel_values(integrand, xgrid, cfg; verbose=true)
+xgrid       = vcat([0.0], collect(range(0.001, 1.1, length=1_000)))
+true_values = [2*ALPHA/(ALPHA^2 + (2*pi*r)^2) for r in xgrid]
+(integrals, errors) = kernel_values(cfg, xgrid; verbose=true)
 
 # relative error w.r.t. K(0) versus error estimate.
-display(hcat(abs.(integrals - true_values)./truth(0.0), errors))
+display(hcat(abs.(integrals - true_values)[2:end]./true_values[1], errors[2:end]))
 
