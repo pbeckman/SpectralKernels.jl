3e353e5e97457586bdc99fea81a4ca3aab112085
Chris Geoga <geoga@wisc.edu>
2026-02-06 15:30:58 -0600

    Clean up testing environment

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 
 using Test, SpectralKernels
 using SpectralKernels.DifferentiationInterface
+using SpectralKernels.LinearAlgebra
+using SpectralKernels.Printf
 
 @testset "Core functionality" begin
     include("exponential_sdf_1d.jl")
